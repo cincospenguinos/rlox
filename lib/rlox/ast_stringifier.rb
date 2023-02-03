effcf2eac094202b6e95a8e6bd7d19c3f3e01c87
Andre LaFleur <cincospenguinos@gmail.com>
2023-02-03 11:03:40 -0700

    Get the stringifier working with a more complex case

--- lib/rlox/ast_stringifier.rb
+++ lib/rlox/ast_stringifier.rb
@@ -1,22 +1,42 @@
 # frozen_string_literal: true
 
 module Rlox
+  ## AstStringifier
+  #
+  # Stringifies expressions for debugging purposes
   class AstStringifier
     def stringify(expression)
       expression.accept(self)
     end
 
     def visit_binary_expr(binary_expr)
-      "(#{binary_expr.operator_token} #{binary_expr.left_expression} #{binary_expr.right_expression})"
+      parenthesize(binary_expr.operator_token.to_s, binary_expr.left_expression,
+        binary_expr.right_expression)
     end
 
     def visit_grouping_expr(grouping_expr)
+      parenthesize('group', grouping_expr.expression)
     end
 
     def visit_literal_expr(literal_expr)
+      return 'nil' if literal_expr.literal_value.type == :nil
+      literal_expr.literal_value.to_s
     end
 
     def visit_unary_expr(unary_expr)
+      parenthesize(unary_expr.operator_token.to_s, unary_expr.right_expression)
+    end
+
+    private
+
+    def parenthesize(expr_name, *exprs)
+      str = "(#{expr_name}"
+
+      exprs.each do |expr|
+        str +=  " #{expr.accept(self)}"
+      end
+
+      "#{str})"
     end
   end
 end
--- test/ast_stringifier_test.rb
+++ test/ast_stringifier_test.rb
@@ -7,11 +7,29 @@ class AstStringifierTest < Test::Unit::TestCase
     stringifier = Rlox::AstStringifier.new
     str = stringifier.stringify(
       Rlox::BinaryExpr.new(
-        Rlox::Token.new(type: :number_literal, string: '1'),
+        Rlox::LiteralExpr.new(Rlox::Token.new(type: :number_literal, string: '1')),
         Rlox::Token.new(type: :plus, string: '+'),
-        Rlox::Token.new(type: :number_literal, string: '2'),
+        Rlox::LiteralExpr.new(Rlox::Token.new(type: :number_literal, string: '2')),
       )
     )
     assert_equal '(+ 1 2)', str
+  end
+
+  test '#stringify handles more complex case' do
+    stringifier = Rlox::AstStringifier.new
+    str = stringifier.stringify(
+      Rlox::BinaryExpr.new(
+        Rlox::UnaryExpr.new(
+          Rlox::Token.new(type: :minus, string: '-'),
+          Rlox::LiteralExpr.new(Rlox::Token.new(type: :number_literal, string: '123')),
+        ),
+        Rlox::Token.new(type: :star, string: '*'),
+        Rlox::GroupingExpr.new(
+          Rlox::LiteralExpr.new(Rlox::Token.new(type: :number_literal, string: '45.67'))
+        )
+      )
+    )
+
+    assert_equal '(* (- 123) (group 45.67))', str
   end
 end
